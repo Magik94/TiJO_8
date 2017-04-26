77b09ff2b417e8dcfd0626a12001a656fcb8a6d0
Szul <dedly04@gmail.com>
2017-04-26 12:32:27 +0200

    test mutants'

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,22 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should return all number',function(){
+        expect(app.reverseNumber(123)).to.eql(321);
+        });
 });
 
 describe('returnOnlyLetter', function ()
 {
+    it('should return string with letters only', function(){
+        expect(app.returnOnlyLetter('ala12')).to.eql('ala');
+    });
 
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true',function(){
+        expect(app.isEmail('lol@o2.pl')).to.eql(true);
+    });
 });
